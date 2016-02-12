4eda5748851ffc77ceb3f1ed105eb9d1c312ddd4
Michael Calkins <michaeljamescalkins@gmail.com>
2016-02-11 21:52:03 -0800

    added a text autoresizer for the title

--- src/browser/index.js
+++ src/browser/index.js
@@ -1,7 +1,9 @@
 'use strict'
 
 let $ = require('jquery'),
-    FileHandler = require('./js/FileHandler.js')
+    FileHandler = require('./js/FileHandler')
+
+require('./js/MenuHandler')
 
 let currentFilename = false
 
@@ -56,54 +58,19 @@ Mousetrap.bind('command+o', (e) => {
         let fileContents = FileHandler.openFile(data[0])
         fileContents = eval('(' + fileContents.toString() + ')')
 
-        document.getElementById('title-content').value = fileContents.title
+        // Resize the title textarea after setting the value
+        $('#title-content').val(fileContents.title).trigger('input')
         document.querySelector("trix-editor").value = fileContents.contents
     })
 })
 
+$(document).ready(function() {
+    // Activates the auto resizing textarea
+    $('#title-content').textareaAutoSize();
 
-
-
-const remote = require('remote');
-const app = remote.app
-const Menu = remote.require('menu');
-const MenuItem = remote.require('menu-item');
-
-const currentWindow = remote.getCurrentWindow();
-
-let rightClickPosition = null;
-
-var template = [{
-    label: "Application",
-    submenu: [
-        { label: "About Application", selector: "orderFrontStandardAboutPanel:" },
-        { type: "separator" },
-        { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
-    ]}, {
-    label: "Edit",
-    submenu: [
-        { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
-        { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
-        { type: "separator" },
-        { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
-        { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
-        { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
-        { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
-    ]}
-];
-
-Menu.setApplicationMenu(Menu.buildFromTemplate(template));
-
-var menu = new Menu();
-menu.append(new MenuItem({ label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" }))
-menu.append(new MenuItem({ label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" }))
-menu.append(new MenuItem({ type: "separator" }))
-menu.append(new MenuItem({ label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" }))
-menu.append(new MenuItem({ label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" }))
-menu.append(new MenuItem({ label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" }))
-menu.append(new MenuItem({ label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }))
-
-window.addEventListener('contextmenu', function (e) {
-  e.preventDefault();
-  menu.popup(remote.getCurrentWindow());
-}, false);
+    // Ensure that the title is at the correct size on focus
+    var ta = document.querySelector('textarea');
+    ta.addEventListener('focus', function(){
+        $('#title-content').trigger('input');
+    });
+})

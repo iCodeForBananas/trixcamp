3381cdb2f3de768cc8c66a37529941b96605458d
Michael Calkins <michaeljamescalkins@gmail.com>
2016-02-14 00:38:21 -0800

    fixed the sticky header bug

--- src/browser/js/StickyHeader.js
+++ src/browser/js/StickyHeader.js
@@ -5,14 +5,16 @@ $(document).ready(function() {
 
     $(window).scroll(function(){
         if ($(window).scrollTop() > stickyRibbonTop) {
+            $('trix-toolbar').addClass('sticky-trix-toolbar')
             $('trix-toolbar').css({
                 borderTopWidth: 0,
                 position: 'fixed', 
                 top: '0px',
                 width: '100%',
-                padding: '0 8rem'
+                padding: '0 6rem 0 8rem'
             })
         } else {
+            $('trix-toolbar').removeClass('sticky-trix-toolbar')
             $('trix-toolbar').css({
                 borderTopWidth: '1px',
                 position: 'static', 
